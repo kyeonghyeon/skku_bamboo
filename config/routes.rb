a70855dc858732aaece4ab0e7cf228ad77b3696a
Kyeonghyeon <myworld0427@gmail.com>
2017-04-10 17:42:58 +0000

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources 'posts'
-  # root 'posts#new'
+  root 'posts#index'
   # get 'posts/index'
   # get 'posts/new', as: 'new'
   # post 'posts' => 'posts#create'
